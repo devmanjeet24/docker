5627b73e6aeb70291fe2ba48da2ca6920cbdc48a
Manjeet Singh <manjeet9871124@gmail.com>
2026-08-22 22:30:20 +0530

    initialize commit

---
+++ level1/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv"
+dotenv.config();
+
+const PORT = process.env.PORT || 8000
+
+const app = express();
+
+
+app.get("/", (req, res) => {
+    return res.status(200).json({message: "hello from docker"});
+})
+
+app.listen(PORT, ()=> {
+    console.log("server started");
+})
